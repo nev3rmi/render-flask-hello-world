3998ac363a0ac3caf3e29396b05b36d162d6a3d2
NeV3RmI <nev3rmi@gmail.com>
2024-02-24 20:19:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import os
-os.environ['PUPPETEER_HOME'] = '/tmp'
-
 from flask import Flask, jsonify, request
 from requests_html import HTMLSession
 import requests
-
-import pyppeteer
-import asyncio
+from threading import Thread
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
@@ -25,12 +20,12 @@ def fetch_url():
     try:
         response = requests.get(url)
         response.raise_for_status()
-    except requests.exceptions.HTTPError as http_err:
-        return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
-    except Exception as err:
-        return jsonify({'error': f'An error occurred: {err}'}), 500
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500
+    
     # Replace all hyphens in header keys with underscores
     modified_headers = {key.replace('-', '_'): value for key, value in response.headers.items()}
+    
     # Build the JSON response with modified headers and content
     output = {
         'headers': dict(response.headers),
@@ -42,6 +37,15 @@ def fetch_url():
     # Return the JSON response
     return jsonify(output)
 
+def fetch_and_render(url, callback):
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        response.html.render()
+        session.close()
+        callback(response)
+    except Exception as e:
+        callback(e)
 
 @app.route('/fetch_html_session', methods=['GET'])
 def fetch_html_session():
@@ -49,40 +53,27 @@ def fetch_html_session():
     url = request.args.get('url')
     if not url:
         return jsonify({'error': 'No URL provided'}), 400
-
-    # Create an HTML Session object
-    session = HTMLSession()
     
-    try:
-        # Fetch the content of the URL as usual
-        response = session.get(url)
-        response.raise_for_status()  # Check for HTTP errors
-        
-        # Render the JavaScript on the page
-        response.html.render()
-        
-        # Since headers might be modified after rendering JavaScript, capture headers again
-        modified_headers = {key.replace('-', '_'): value for key, value in response.headers.items()}
+    results = {}
 
-        # Build the JSON response with data from the rendered page
-        output = {
-            'headers': dict(response.headers),
-            'modified_headers': modified_headers,
-            'content': response.html.html,
-            'status_code': response.status_code,
-            'url': response.url
-        }
-        
-    except requests.exceptions.HTTPError as http_err:
-        return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
-    except Exception as err:
-        return jsonify({'error': f'An error occurred: {err}'}), 500
-    finally:
-        # Make sure to close the session
-        session.close()
+    def on_result(response):
+        if isinstance(response, Exception):
+            results['error'] = str(response)
+        else:
+            results.update({
+                'headers': dict(response.headers),
+                'content': response.html.html,
+                'status_code': response.status_code,
+                'url': response.url
+            })
     
-    # Return the JSON response
-    return jsonify(output)
-
-# You may need to set up a custom event loop policy to prevent event loop policy errors
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy() if os.name == 'nt' else asyncio.DefaultEventLoopPolicy())
+    # Create a Thread to fetch and render the page in the background
+    thread = Thread(target=fetch_and_render, args=(url, on_result))
+    thread.start()
+    thread.join()  # Wait for the thread to finish
+    
+    if 'error' in results:
+        return jsonify({'error': results['error']}), 500
+    
+    # Return the JSON response with the rendered content
+    return jsonify(results)
